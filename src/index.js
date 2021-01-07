d20f23cf890c02fe004e83eff3d9efe7d3085477
korytsa <anyakorytko@mail.ru>
2021-01-07 20:49:49 +0300

    add: first page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './scss/app.scss';
 import './js/bots-list.js';
 import './js/questions-list.js';
-
+import './js/entrance.js';
---
+++ src/js/entrance.js
@@ -0,0 +1,13 @@
+const markup = `
+<div class="wrapper">
+    <div class="entrance">
+        <h2 class="entrance__title">SIGame Online</h2>
+        <p class="entrance__name">Ваше имя</p>
+        <input class="entrance__input">
+    </div>
+    <div class="entrance__button">
+        <button class="entrance__btn">Вход</button>
+    </div>
+</div>
+`;
+document.body.innerHTML = markup;
