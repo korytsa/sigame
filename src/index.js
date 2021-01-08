9dac359211e3b33de7fa47069b3ff82355c214c0
korytsa <anyakorytko@mail.ru>
2021-01-08 03:27:51 +0300

    add: second page

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './scss/app.scss';
 import './js/bots-list.js';
 import './js/questions-list.js';
 import './js/entrance.js';
+import './js/game-selection.js';
---
+++ src/js/game-selection.js
@@ -0,0 +1,9 @@
+const pageTwo = `
+<div class="game_selection">
+    <h2 class="game_selection__title">Выберите действие</h2>
+    <ul>
+        <li>Играть с ботами</li>
+    </ul>
+</div>
+`;
+document.body.innerHTML = pageTwo;
