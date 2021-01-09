e9cfa57d92ffbd7a871a35f7bc683f13cf89fc2d
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-01-09 16:09:06 +0400

    change some files and add app-logic js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import './js/bots-list.js';
 import './js/questions-list.js';
 import './js/render-game-screen.js';
 import './js/change-screen-content.js';
+import './js/app-logic.js';
--- src/js/change-screen-content.js
+++ src/js/change-screen-content.js
@@ -1,35 +1,26 @@
+import {questionPack} from './questions-list'
+
 const changeContent = {
-    mainScreen: document.querySelector('.game__main-area__main-screen'),
-    subScreen: document.querySelector('.game__sub-screen__display'),
-    clearMainScreen() {
-        const mainScreen = changeContent.mainScreen;
-        while (mainScreen.firstChild) {
-            mainScreen.firstChild.remove();
-        }
-    },
-    clearSubScreen() {
-        const subScreen = changeContent.subScreen;
-        while (subScreen.firstChild) {
-            subScreen.firstChild.remove();
+    clearScreen(currentScreen) {
+        while (currentScreen.firstChild) {
+            currentScreen.firstChild.remove();
         }
     },
     // пока реализвация только текстовых вопросов
-    insertMainScreenContent(content) {
-        const mainScreen = changeContent.mainScreen;
-        changeContent.clearMainScreen();
-        
-        const screenContent = document.createElement('p');
-        screenContent.className = 'game__main-screen__text';
-        screenContent.innerText = content;
-        mainScreen.append(screenContent);
-    },
-    insertSubScreenContent(content) {
-        const subScreen = changeContent.subScreen;
-        changeContent.clearSubScreen();
+    insertScreenContent(currentScreen, content) {
+        changeContent.clearScreen(currentScreen);
 
         const screenContent = document.createElement('p');
-        screenContent.className = 'game__sub-screen__display__text';
+        if (currentScreen.className === 'game__main-area__main-screen') {
+            screenContent.className = 'game__main-screen__text';
+        } else {
+            screenContent.className = 'game__sub-screen__display__text';
+        }
         screenContent.innerText = content;
-        subScreen.append(screenContent);
+        currentScreen.append(screenContent);
     },
+    theme: `Тема: ${questionPack[0].theme}`,
+    author: `Автор темы: ${questionPack[0].author}`
 }
+
+export {changeContent};
