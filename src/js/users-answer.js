34414fd77da0ba91d17476d5b1318d9be8e3472d
korytsa <anyakorytko@mail.ru>
2021-01-17 19:14:06 +0300

    add: line and text

--- src/js/users-answer.js
+++ src/js/users-answer.js
@@ -24,4 +24,54 @@ const inputField = {
     }
 }
 
+const line = {
+    renderQuestion() {
+        const mainScreen = document.querySelector('.game__main-area__main-screen');
+
+        const firstLine = document.createElement("p");
+        firstLine.className = "line__first";
+        mainScreen.append(firstLine);
+
+        const center = document.createElement('div');
+        center.className = "line__center"
+        mainScreen.append(center);
+
+        const secondLine = document.createElement("div");
+        secondLine.className = "line__second";
+        center.append(secondLine);
+
+        const container = document.createElement('div');
+        container.className = 'line__container';
+        center.appendChild(container);
+
+        const letterContainer = document.createElement('p');
+        letterContainer.className = 'line__letter-container';
+        letterContainer.innerText = 'Example!';
+        container.appendChild(letterContainer);
+
+        const fourthLine = document.createElement("div");
+        fourthLine.className = "line__fourth";
+        center.append(fourthLine);
+
+        const thirdLine = document.createElement("p");
+        thirdLine.className = "line__third";
+        mainScreen.append(thirdLine);
+
+        letterContainer.style.display = "none";
+
+        for (var i = 0; i < letterContainer.textContent.length; i++) {
+        (function(i) {
+            setTimeout(function() {
+                var texts = document.createTextNode(letterContainer.textContent[i])
+                var span = document.createElement('span');
+                span.appendChild(texts);
+
+                span.classList.add("line__letter");
+                container.appendChild(span);
+
+            }, 115 * i);}(i));
+        }
+    }
+}
+export {line};
 export {inputField};
