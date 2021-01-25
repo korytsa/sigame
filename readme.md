f3eb26a98ca947713a249ec9ac4a26cc0def8d98
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-01-25 14:44:08 +0400

    doc: add readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+1. git clone from github
+2. npm i
+3. npm run start
+4. Start live server
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './scss/app.scss';
-import './scss/render-game-screen.scss';
-import './scss/change-screen-content.scss';
-import './js/bots-list.js';
-import './js/questions-list.js';
-import './js/render-game-screen.js';
-import './js/change-screen-content.js';
-import './js/app-logic.js';
-import './js/question.js';
